affd7940ad10e27f53bca164ccc7d8b0a016da35
evg-kazartseff <evg.kazartseff@yandex.ru>
2016-04-03 18:34:51 +0600

    lab2 debug

--- lab2/1.c
+++ lab2/1.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-void init(int *arr, int n)
+
+void init(int **arr, int n)
 {
- arr = malloc(n * sizeof(int));
- int i;
- for (i = 0; i < n; ++i)
- {
- arr[i] = i;
- }
+    int* tmp = malloc(n * sizeof(int));
+    int i;
+    for (i = 0; i < n; ++i) {
+        tmp[i] = i;
+    }
+    *arr =tmp;
 }
 int main()
 {
- int* arr = NULL;
- int n = 10;
- init(arr, n);
- int i;
- for (i = 0; i < n; ++i)
- {
- printf("%d\n", arr[i]);
- }
- return 0;
+    int *arr = NULL;
+    int n = 10;
+    init(&arr, n);
+    int i;
+    for (i = 0; i < n; ++i) {
+        printf("%d\n", arr[i]);
+    }
+    return 0;
 } 
